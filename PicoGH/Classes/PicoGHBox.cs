33eb92ec987200b59eff38b0d67790f8000019f7
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-03-12 22:27:50 +0000

    Moving around

---
+++ PicoGH/Classes/PicoGHBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap71.ShapeKernel;
+using PicoGH.Interfaces;
+using PicoGH.PicoGH.Types;
+
+namespace PicoGH.PicoGH.Classes
+{
+    public class PicoGHBox : PicoGHVoxels, IModulate
+    {
+        BaseBox _basebox;
+
+        public PicoGHBox(BaseBox basebox)
+        {
+            _basebox = basebox;
+            _pmesh = _basebox.mshConstruct();
+            _pvoxels = _basebox.voxConstruct();
+
+            _rmesh = Utilities.PicoMeshToRhinoMesh(_pmesh);
+        }
+
+        public PicoGHVoxels SetModulation(PicoGHModulation modulation1, PicoGHModulation modulation2)
+        {
+
+            PicoGHBox output = new PicoGHBox(_basebox);
+
+            output._basebox.SetWidth(modulation1._lModulation);
+            output._basebox.SetDepth(modulation2._lModulation);
+
+            output._basebox.SetWidthSteps(100);
+            output._basebox.SetDepthSteps(100);
+
+            return output;
+        }
+    }
+}
---
+++ PicoGH/Classes/PicoGHPipeSegment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Leap71.ShapeKernel;
+using PicoGH.Interfaces;
+using PicoGH.PicoGH.Types;
+
+namespace PicoGH.Types
+{
+    public class PicoGHPipeSegment : PicoGHVoxels, IModulate
+    {
+        public BasePipeSegment _pipeSegment;
+
+        public PicoGHPipeSegment(BasePipeSegment pipeSegment)
+        {
+            _pipeSegment = pipeSegment;
+            _pvoxels = _pipeSegment.voxConstruct();
+            _pmesh = _pipeSegment.mshConstruct();
+
+            _rmesh = Utilities.PicoMeshToRhinoMesh( _pmesh );
+        }
+
+        public PicoGHVoxels SetModulation(PicoGHModulation modulation1, PicoGHModulation modulation2)
+        {
+            SurfaceModulation surfMod1;
+            SurfaceModulation surfMod2;
+
+            if ((modulation1._lModulation is null) & (modulation2._lModulation is null))
+            {
+                surfMod1 = modulation1._sModulation;
+                surfMod2 = modulation2._sModulation;
+            }
+            else
+            {
+                surfMod1 = new SurfaceModulation(modulation1._lModulation);
+                surfMod2 = new SurfaceModulation(modulation2._lModulation);
+            }
+
+            PicoGHPipeSegment output = new PicoGHPipeSegment(_pipeSegment);
+
+            output._pipeSegment.SetRadius(surfMod1, surfMod2);
+            output._pipeSegment.SetLengthSteps(100);
+            
+            return output;
+        }
+    }
+}
