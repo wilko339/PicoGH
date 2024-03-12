c228d5105f1411c1a848eee252f5c244cbdec03f
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-03-12 22:28:01 +0000

    Init commit

---
+++ PicoGH/Interfaces/IModulate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PicoGH.PicoGH.Types;
+
+namespace PicoGH.Interfaces
+{
+    public interface IModulate
+    {
+        PicoGHVoxels SetModulation(PicoGHModulation modulation1, PicoGHModulation modulation2);
+    }
+}
