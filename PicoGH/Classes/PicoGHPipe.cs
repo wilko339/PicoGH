5e7a3a8aaa586d31506cf50c2d768ec99c824546
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-03-12 23:03:03 +0000

    Init commit

---
+++ PicoGH/Classes/PicoGHPipe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap71.ShapeKernel;
+using PicoGH.Interfaces;
+using PicoGH.PicoGH.Types;
+
+namespace PicoGH.PicoGH.Classes
+{
+    public class PicoGHPipe : PicoGHVoxels, IModulate
+    {
+        BasePipe _pipe;
+        public PicoGHPipe(BasePipe pipe)
+        {
+            _pipe = pipe;
+
+            _pmesh = pipe.mshConstruct();
+            _pvoxels = pipe.voxConstruct();
+
+            _rmesh = Utilities.PicoMeshToRhinoMesh(_pmesh);
+        }
+        public PicoGHVoxels SetModulation(PicoGHModulation modulation1, PicoGHModulation modulation2)
+        {
+            PicoGHPipe output = new PicoGHPipe(_pipe);
+
+            SurfaceModulation innerMod;
+            SurfaceModulation outerMod;
+
+            if (modulation1._sModulation is null &  modulation2._sModulation is null)
+            {
+                innerMod = new SurfaceModulation(modulation1._lModulation);
+                outerMod = new SurfaceModulation(modulation2._lModulation);
+            }
+            else
+            {
+                innerMod = modulation1._sModulation;
+                outerMod = modulation2._sModulation;
+            }
+
+            output._pipe.SetRadius(innerMod, outerMod);
+            output._pipe.SetLengthSteps(100);
+
+            return output;
+        }
+    }
+}
---
+++ PicoGH/Utilities/PicoGHConfig.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using PicoGK;
+using Rhino.Geometry;
+
+namespace PicoGH
+{
+    public class PicoGHConfig : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the PicoGHConfig class.
+        /// </summary>
+        public PicoGHConfig()
+          : base("PicoGHConfig", "Config",
+              "Sets some options for PicoGK",
+              "PicoGH", "Config")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("VoxelSize", "V", "Sets the global voxel size for PicoGK.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            GH_Number voxelSize = new GH_Number();
+            if (!DA.GetData(0, ref voxelSize)) return;
+
+            Library.SetVoxelSize((float)voxelSize.Value);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5EF191F8-14D0-48C3-B30E-CB526D858EA1"); }
+        }
+    }
+}
