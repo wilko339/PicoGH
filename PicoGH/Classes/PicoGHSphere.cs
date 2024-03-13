3eb8d5b26dfda0c4f2cc3cef8eaadaec8468a095
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-03-13 22:28:23 +0000

    Initial commit

---
+++ PicoGH/Classes/PicoGHSphere.cs
@@ -0,0 +1,26 @@
+﻿using PicoGK;
+using Leap71.ShapeKernel;
+
+namespace PicoGH.Classes
+{
+    public class PicoGHSphere : PicoGHVoxels
+    {
+        BaseSphere _BaseSphere;
+
+        public PicoGHSphere(BaseSphere baseSphere)
+        {
+            _BaseSphere = baseSphere;
+            RMesh = Utilities.PicoMeshToRhinoMesh(GeneratePMesh());
+        }
+
+        public override Mesh GeneratePMesh()
+        {
+            return _BaseSphere.mshConstruct();
+        }
+
+        public override Voxels GenerateVoxels()
+        {
+            return _BaseSphere.voxConstruct();
+        }
+    }
+}
