c7eddc91080ebea871b9fa1181e912833c44ff80
Toby Wilkinson <toby.wilkinson339@gmail.com>
2024-03-19 22:12:30 +0000

    Removed some usings

--- PicoGH/Utilities/Utilities.cs
+++ PicoGH/Utilities/Utilities.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using Leap71.ShapeKernel;
 using PicoGK;
 using Rhino.Geometry;
-using Rhino.Render.ChangeQueue;
 
 namespace PicoGH
 {
